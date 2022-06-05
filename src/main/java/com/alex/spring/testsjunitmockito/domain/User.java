85bd7d9ecd02cdb130a64bbd83d9bfcc4306b6b0
Alexsander <alexsander_lima99@hotmail.com>
2022-06-05 10:53:16 -0300

    Domain

--- src/main/java/com/alex/spring/testsjunitmockito/domain/User.java
+++ src/main/java/com/alex/spring/testsjunitmockito/domain/User.java
@@ -1,4 +1,22 @@
 package com.alex.spring.testsjunitmockito.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@EqualsAndHashCode
+@Builder
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
 }
