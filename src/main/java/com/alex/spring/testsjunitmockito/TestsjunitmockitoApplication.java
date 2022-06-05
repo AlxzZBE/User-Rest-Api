3ad7c244e984b65a6fb2d5522411c18dd1988b78
Alexsander <alexsander_lima99@hotmail.com>
2022-06-05 10:30:18 -0300

    First Commit

---
+++ src/main/java/com/alex/spring/testsjunitmockito/TestsjunitmockitoApplication.java
@@ -0,0 +1,13 @@
+package com.alex.spring.testsjunitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsjunitmockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestsjunitmockitoApplication.class, args);
+	}
+
+}
