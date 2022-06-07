4e50c7600c7468bd0759f92f3800a572b7e1a4af
Alexsander <alexsander_lima99@hotmail.com>
2022-06-07 19:37:48 -0300

    Test for UserController findAll both returns

--- src/test/java/com/alex/spring/testsjunitmockito/controller/UserControllerTest.java
+++ src/test/java/com/alex/spring/testsjunitmockito/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.alex.spring.testsjunitmockito.controller;
 
+import com.alex.spring.testsjunitmockito.exceptions.NotFoundException;
 import com.alex.spring.testsjunitmockito.requests.UserGet;
 import com.alex.spring.testsjunitmockito.services.UserService;
 import com.alex.spring.testsjunitmockito.util.UserCreator;
@@ -17,6 +18,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Collections;
+import java.util.List;
+
 @ExtendWith(SpringExtension.class)
 class UserControllerTest {
 
@@ -25,16 +29,20 @@ class UserControllerTest {
     public static final String EXPECTED_NAME = UserGetCreator.createValidUserGet().getName();
     public static final String EXPECTED_EMAIL = UserGetCreator.createValidUserGet().getEmail();
 
+    public static final String USER_WITH_ID_NOT_FOUND = "User with id `%d` not found";
+
     @InjectMocks
     private UserController userController;
 
     @Mock
-    private UserService userService;
+    private UserService userServiceMock;
 
     @BeforeEach
     void setUp() {
-        BDDMockito.when(userService.findByIdOrThrowNotFoundException(ArgumentMatchers.anyInt()))
+        BDDMockito.when(userServiceMock.findByIdOrThrowNotFoundException(ArgumentMatchers.anyInt()))
                 .thenReturn(UserCreator.createValidUser());
+
+        BDDMockito.when(userServiceMock.findAll()).thenReturn(List.of(UserCreator.createValidUser()));
     }
 
     @Test
@@ -51,7 +59,38 @@ class UserControllerTest {
     }
 
     @Test
-    void findAll() {
+    @DisplayName("findById Throws NotFoundException When User Is Not Found")
+    void findById_ThrowsNotFoundException_WhenUserIsNotFound() {
+        BDDMockito.when(userServiceMock.findByIdOrThrowNotFoundException(ArgumentMatchers.anyInt()))
+                .thenThrow(new NotFoundException(USER_WITH_ID_NOT_FOUND.formatted(ID1)));
+
+        Assertions.assertThatExceptionOfType(NotFoundException.class)
+                .isThrownBy(() -> userController.findById(ID1))
+                .withMessageContainingAll(USER_WITH_ID_NOT_FOUND.formatted(ID1));
+    }
+
+    @Test
+    @DisplayName("findAll Returns a List Of UserGet When Successful")
+    void findAll_ReturnsAListOfUsersGet_WhenSuccessful() {
+        ResponseEntity<List<UserGet>> userGetResponseList = userController.findAll();
+
+        Assertions.assertThat(userGetResponseList.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(userGetResponseList.getBody()).isNotNull().hasSize(1);
+        Assertions.assertThat(userGetResponseList.getBody().get(0)).isNotNull().isExactlyInstanceOf(UserGet.class);
+        Assertions.assertThat(userGetResponseList.getBody().get(0).getId()).isNotNull().isEqualTo(EXPECTED_ID);
+        Assertions.assertThat(userGetResponseList.getBody().get(0).getName()).isNotNull().isEqualTo(EXPECTED_NAME);
+        Assertions.assertThat(userGetResponseList.getBody().get(0).getEmail()).isNotNull().isEqualTo(EXPECTED_EMAIL);
+    }
+
+    @Test
+    @DisplayName("findAll Returns Empty List When User Is Not Found")
+    void findAll_ReturnsEmptyList_WhenUserIsNotFound() {
+        BDDMockito.when(userServiceMock.findAll()).thenReturn(Collections.emptyList());
+
+        ResponseEntity<List<UserGet>> userGetResponseList = userController.findAll();
+
+        Assertions.assertThat(userGetResponseList.getStatusCode()).isNotNull().isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(userGetResponseList.getBody()).isNotNull().isEmpty();
     }
 
     @Test
